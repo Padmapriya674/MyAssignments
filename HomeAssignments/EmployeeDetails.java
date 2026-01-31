ef9f50990c1f9c9cfe9f6e69cd1d82709db9f1eb
Padmapriya674 <vpadmapriya31@gmail.com>
2026-01-31 22:12:56 +0530

    Add files via upload
    Week1 Day1

---
+++ HomeAssignments/EmployeeDetails.java
@@ -0,0 +1,33 @@
+package HomeAssignments;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		String mobileNumber = "9876543210";
+		int pinCode = 56001;
+		int luckyNumber = 7;
+				
+					System.out.println("Name is "+name);
+					System.out.println("Age is " + age);
+					System.out.println("Salar is " +salary);
+					System.out.println("Gender is " +  gender);
+					System.out.println("Accept Terms is " +  acceptTerms);
+					System.out.println("Rating is " +  rating);
+					System.out.println("Mob no " +  mobileNumber);
+					System.out.println("Pincode is "+  pinCode);
+					System.out.println("Lucky Number is" +  luckyNumber);
+
+		}
+
+	}
+
+
+
---
+++ week1/day1/LearnDatatype.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class LearnDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive datatypes
+				byte noOfLearners=50;
+				float speed=0.0234676766767f;
+				double speed1=1.6568786973655;
+				long pHno=456567676764L;
+				//System.out.println(speed);
+				System.out.println("speed of car:"+speed1+"\n"+"speed of a machine:"+speed);
+				char logo='S';
+				char logoNum='5';
+				boolean type=true;
+				//Nonprimitive datatypes
+				String s="Hello everyone,Welcome to first session of this selenium course";
+				System.out.println("The given String is:"+s+","+"phone Number is:"+pHno);
+
+			}
+
+		
+	}
+
+
